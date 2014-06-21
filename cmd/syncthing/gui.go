a24bcf27e1748488d3cf4828462832a0d5ea5140
Jakob Borg <jakob@nym.se>
2014-06-21 13:50:02 +0200

    wip

--- cmd/syncthing/gui.go
+++ cmd/syncthing/gui.go
@@ -87,47 +87,55 @@ func startGUI(cfg config.GUIConfiguration, assetDir string, m *model.Model) erro
 		static = embeddedStatic()
 	}
 
-	router := martini.NewRouter()
-	router.Get("/", getRoot)
-	router.Get("/rest/version", restGetVersion)
-	router.Get("/rest/model", restGetModel)
-	router.Get("/rest/model/version", restGetModelVersion)
-	router.Get("/rest/need", restGetNeed)
-	router.Get("/rest/connections", restGetConnections)
-	router.Get("/rest/config", restGetConfig)
-	router.Get("/rest/config/sync", restGetConfigInSync)
-	router.Get("/rest/system", restGetSystem)
-	router.Get("/rest/errors", restGetErrors)
-	router.Get("/rest/discovery", restGetDiscovery)
-	router.Get("/rest/report", restGetReport)
-	router.Get("/qr/:text", getQR)
+	getRestMux := http.NewServeMux()
+	getRestMux.HandleFunc("/rest/version", restGetVersion)
+	getRestMux.HandleFunc("/rest/model", withModel(m, restGetModel))
+	getRestMux.HandleFunc("/rest/model/version", withModel(m, restGetModelVersion))
+	getRestMux.HandleFunc("/rest/need", withModel(m, restGetNeed))
+	getRestMux.HandleFunc("/rest/connections", withModel(m, restGetConnections))
+	getRestMux.HandleFunc("/rest/config", restGetConfig)
+	getRestMux.HandleFunc("/rest/config/sync", restGetConfigInSync)
+	getRestMux.HandleFunc("/rest/system", restGetSystem)
+	getRestMux.HandleFunc("/rest/errors", restGetErrors)
+	getRestMux.HandleFunc("/rest/discovery", restGetDiscovery)
+	getRestMux.HandleFunc("/rest/report", withModel(m, restGetReport))
 
-	router.Post("/rest/config", restPostConfig)
-	router.Post("/rest/restart", restPostRestart)
-	router.Post("/rest/reset", restPostReset)
-	router.Post("/rest/shutdown", restPostShutdown)
-	router.Post("/rest/error", restPostError)
-	router.Post("/rest/error/clear", restClearErrors)
-	router.Post("/rest/discovery/hint", restPostDiscoveryHint)
-	router.Post("/rest/model/override", restPostOverride)
+	postRestMux := http.NewServeMux()
+	postRestMux.HandleFunc("/rest/config", withModel(m, restPostConfig))
+	postRestMux.HandleFunc("/rest/restart", restPostRestart)
+	postRestMux.HandleFunc("/rest/reset", restPostReset)
+	postRestMux.HandleFunc("/rest/shutdown", restPostShutdown)
+	postRestMux.HandleFunc("/rest/error", restPostError)
+	postRestMux.HandleFunc("/rest/error/clear", restClearErrors)
+	postRestMux.HandleFunc("/rest/discovery/hint", restPostDiscoveryHint)
+	postRestMux.HandleFunc("/rest/model/override", withModel(m, restPostOverride))
 
-	mr := martini.New()
-	mr.Use(csrfMiddleware)
-	if len(cfg.User) > 0 && len(cfg.Password) > 0 {
-		mr.Use(basic(cfg.User, cfg.Password))
-	}
-	mr.Use(static)
-	mr.Use(martini.Recovery())
-	mr.Use(restMiddleware)
-	mr.Action(router.Handle)
-	mr.Map(m)
+	restMux := noCache(getPostHandler(getRestMux, postRestMux))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("gui")))
+	mux.HandleFunc("/rest/", restMux)
+	mux.HandleFunc("/qr/", getQR)
 
 	apiKey = cfg.APIKey
 	loadCsrfTokens()
 
-	go http.Serve(listener, mr)
+	go http.Serve(listener, mux)
 
 	return nil
+}
+
+func getPostHandler(get, post http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			get.ServeHTTP(w, r)
+		case "POST":
+			post.ServeHTTP(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	}
 }
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -135,14 +143,21 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	static(w, r, nil)
 }
 
-func restMiddleware(w http.ResponseWriter, r *http.Request) {
-	if len(r.URL.Path) >= 6 && r.URL.Path[:6] == "/rest/" {
+func noCache(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-cache")
+		h(w, r)
 	}
 }
 
-func restGetVersion() string {
-	return Version
+func withModel(m *model.Model, h func(m *model.Model, w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h(m, w, r)
+	}
+}
+
+func restGetVersion(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(Version))
 }
 
 func restGetModelVersion(m *model.Model, w http.ResponseWriter, r *http.Request) {
@@ -186,7 +201,7 @@ func restGetModel(m *model.Model, w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res)
 }
 
-func restPostOverride(m *model.Model, r *http.Request) {
+func restPostOverride(m *model.Model, w http.ResponseWriter, r *http.Request) {
 	var qs = r.URL.Query()
 	var repo = qs.Get("repo")
 	m.Override(repo)
@@ -202,13 +217,13 @@ func restGetNeed(m *model.Model, w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(files)
 }
 
-func restGetConnections(m *model.Model, w http.ResponseWriter) {
+func restGetConnections(m *model.Model, w http.ResponseWriter, r *http.Request) {
 	var res = m.ConnectionStats()
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(w).Encode(res)
 }
 
-func restGetConfig(w http.ResponseWriter) {
+func restGetConfig(w http.ResponseWriter, r *http.Request) {
 	encCfg := cfg
 	if encCfg.GUI.Password != "" {
 		encCfg.GUI.Password = unchangedPassword
@@ -217,7 +232,7 @@ func restGetConfig(w http.ResponseWriter) {
 	json.NewEncoder(w).Encode(encCfg)
 }
 
-func restPostConfig(req *http.Request, m *model.Model) {
+func restPostConfig(m *model.Model, w http.ResponseWriter, req *http.Request) {
 	var newCfg config.Configuration
 	err := json.NewDecoder(req.Body).Decode(&newCfg)
 	if err != nil {
@@ -289,26 +304,22 @@ func restPostConfig(req *http.Request, m *model.Model) {
 	}
 }
 
-func restGetConfigInSync(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restGetConfigInSync(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]bool{"configInSync": configInSync})
 }
 
-func restPostRestart(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restPostRestart(w http.ResponseWriter, r *http.Request) {
 	flushResponse(`{"ok": "restarting"}`, w)
 	go restart()
 }
 
-func restPostReset(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restPostReset(w http.ResponseWriter, r *http.Request) {
 	flushResponse(`{"ok": "resetting repos"}`, w)
 	resetRepositories()
 	go restart()
 }
 
-func restPostShutdown(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restPostShutdown(w http.ResponseWriter, r *http.Request) {
 	flushResponse(`{"ok": "shutting down"}`, w)
 	go shutdown()
 }
@@ -322,7 +333,7 @@ func flushResponse(s string, w http.ResponseWriter) {
 var cpuUsagePercent [10]float64 // The last ten seconds
 var cpuUsageLock sync.RWMutex
 
-func restGetSystem(w http.ResponseWriter) {
+func restGetSystem(w http.ResponseWriter, r *http.Request) {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 
@@ -347,20 +358,19 @@ func restGetSystem(w http.ResponseWriter) {
 	json.NewEncoder(w).Encode(res)
 }
 
-func restGetErrors(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restGetErrors(w http.ResponseWriter, r *http.Request) {
 	guiErrorsMut.Lock()
 	json.NewEncoder(w).Encode(guiErrors)
 	guiErrorsMut.Unlock()
 }
 
-func restPostError(req *http.Request) {
+func restPostError(w http.ResponseWriter, req *http.Request) {
 	bs, _ := ioutil.ReadAll(req.Body)
 	req.Body.Close()
 	showGuiError(0, string(bs))
 }
 
-func restClearErrors() {
+func restClearErrors(w http.ResponseWriter, r *http.Request) {
 	guiErrorsMut.Lock()
 	guiErrors = []guiError{}
 	guiErrorsMut.Unlock()
@@ -375,7 +385,7 @@ func showGuiError(l logger.LogLevel, err string) {
 	guiErrorsMut.Unlock()
 }
 
-func restPostDiscoveryHint(r *http.Request) {
+func restPostDiscoveryHint(w http.ResponseWriter, r *http.Request) {
 	var qs = r.URL.Query()
 	var node = qs.Get("node")
 	var addr = qs.Get("addr")
@@ -384,18 +394,18 @@ func restPostDiscoveryHint(r *http.Request) {
 	}
 }
 
-func restGetDiscovery(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restGetDiscovery(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(discoverer.All())
 }
 
-func restGetReport(w http.ResponseWriter, m *model.Model) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+func restGetReport(m *model.Model, w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(reportData(m))
 }
 
-func getQR(w http.ResponseWriter, params martini.Params) {
-	code, err := qr.Encode(params["text"], qr.M)
+func getQR(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	text := r.FormValue("text")
+	code, err := qr.Encode(text, qr.M)
 	if err != nil {
 		http.Error(w, "Invalid", 500)
 		return
